b6359f35422f72fe637f51817501bccbd4ce93aa
ozmikhail <237983078+ozmikhail@users.noreply.github.com>
2021-09-03 18:27:42 +0300

    feat(gradebook): add subject and student container maps

--- core/src/gradebook.cpp
+++ core/src/gradebook.cpp
@@ -1,3 +1,9 @@
 #include "gradebook.hpp"
 
-Gradebook::Gradebook() = default;
+bool Gradebook::hasSubject(const std::string& name) const {
+    return m_subjects.find(name) != m_subjects.end();
+}
+
+bool Gradebook::hasStudent(const std::string& id) const {
+    return m_students.find(id) != m_students.end();
+}
--- core/src/gradebook.hpp
+++ core/src/gradebook.hpp
@@ -1,6 +1,20 @@
 #pragma once
+#include "student.hpp"
+#include <map>
+#include <string>
 
 class Gradebook {
 public:
-    Gradebook();
+    const std::map<std::string, Subject>& subjects() const { return m_subjects; }
+    const std::map<std::string, Student>& students() const { return m_students; }
+    std::map<std::string, Subject>& subjects() { return m_subjects; }
+    std::map<std::string, Student>& students() { return m_students; }
+
+    bool hasSubject(const std::string& name) const;
+    bool hasStudent(const std::string& id) const;
+    bool empty() const { return m_subjects.empty() && m_students.empty(); }
+
+private:
+    std::map<std::string, Subject> m_subjects;
+    std::map<std::string, Student> m_students;
 };
